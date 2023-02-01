5448ac0a88c140d1e4a353f0489aab0a16c9a462
sarahitsko <112071488+sarahitsko@users.noreply.github.com>
2023-02-01 22:49:18 +0200

    more css

--- team-8-game/src/components/board.js
+++ team-8-game/src/components/board.js
@@ -1,58 +1,78 @@
-import React, { memo, useEffect, useRef, useContext} from 'react';
-import {useBoard} from "./useBoard.js";
-import { appContext } from '../appContext.js';
-import axios from 'axios'
+import React, { memo, useEffect, useRef, useContext } from "react";
+import { useBoard } from "./useBoard.js";
+import { appContext } from "../appContext.js";
+import axios from "axios";
 
 const Board = () => {
+  const { user } = useContext(appContext);
+  const [display, score, onKeyDown, level] = useBoard();
+  const eBoard = useRef();
 
-    const {user} = useContext(appContext)
-    const [display, score, onKeyDown, level] = useBoard();
-    const eBoard = useRef();
-    
+  useEffect(focusBoard, []);
 
-    useEffect(focusBoard, []);
+  function focusBoard() {
+    eBoard.current.focus();
+  }
 
-    function focusBoard() {
-        eBoard.current.focus();
-    }
+  function sendScore(e) {
+    e.preventDefault();
+    axios.post(`http://localhost:8080/scores/Light`, { user, score });
+    alert("Score Saved");
+  }
 
-    function sendScore(e) {
-        e.preventDefault()
-        axios.post(`http://localhost:8080/scores/Light`, {user,score});
-        alert("Score Saved") }
-
-    return (
-        <>
-        <div  ref={eBoard} className={'t-board'} tabIndex={0} onKeyDown={ onKeyDown }>
-            <div>
-                <span className="t-score-label">Score: </span>
-                <span className="t-score-label">{score.toLocaleString()}</span>
-            </div>
-            {display.map( (row, index) => <Row row={row} key={index}/>)}
-            <span className="t-score-label"> {level}</span><span><button onClick={sendScore} className="btn btn-primary">Save Score</button></span>
-            
+  return (
+    <>
+      <div
+        ref={eBoard}
+        className={"t-board"}
+        tabIndex={0}
+        onKeyDown={onKeyDown}
+      >
+        <div>
+          <span className="t-score-label">Score: </span>
+          <span className="t-score-label">{score.toLocaleString()}</span>
         </div>
-        <h3>Up Arrow will change the direction of the Shape, SpaceBar will drop the Shape 10 squares.</h3>
-        </>
-    );
+        {display.map((row, index) => (
+          <Row row={row} key={index} />
+        ))}
+        <span className="t-score-label"> {level}</span>
+        <span>
+          <button
+            onClick={sendScore}
+            className="btn btn-primary"
+            style={{
+              backgroundColor: " rgb(49, 96, 73)",
+              borderColor: " rgb(49, 96, 73)",
+            }}
+          >
+            Save Score
+          </button>
+        </span>
+      </div>
+      <h3>
+        Up Arrow will change the direction of the Shape, SpaceBar will drop the
+        Shape 10 squares.
+      </h3>
+    </>
+  );
 };
 
-const Row = memo( props => {
-    return (
-        <span className='t-row'>
-            {props.row.map( (cell, index) => <Cell cell={cell} key={index}/>)}
-        </span>
-    );
+const Row = memo((props) => {
+  return (
+    <span className="t-row">
+      {props.row.map((cell, index) => (
+        <Cell cell={cell} key={index} />
+      ))}
+    </span>
+  );
 });
 
-const Cell = memo( props => {
-    const count = useRef(0);
-    count.current++;
+const Cell = memo((props) => {
+  const count = useRef(0);
+  count.current++;
 
-    const value = props.cell ? props.cell : 0;
-    return (
-        <span className={`t-cell t-cell-${value}`}></span>
-    );
+  const value = props.cell ? props.cell : 0;
+  return <span className={`t-cell t-cell-${value}`}></span>;
 });
 
 export default memo(Board);
